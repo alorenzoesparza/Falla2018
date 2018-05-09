b575ce8d0b01059a68b4c878d856917c089f9bbb
Antonio <alorenzoesparza@gmail.com>
2018-05-09 13:14:58 +0200

    Backend

---
+++ Falla2018.Backend/Models/LocalDataContext.cs
@@ -0,0 +1,9 @@
+﻿namespace Falla2018.Backend.Models
+{
+    using Domain;
+
+    public class LocalDataContext : DataContext
+    {
+        public System.Data.Entity.DbSet<Falla2018.Domain.User> Users { get; set; }
+    }
+}
